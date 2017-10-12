2a127fb42f3820a53849d4eff790de86cb623f5f
Florian Reiter <flow.reiter@gmail.com>
2017-10-12 15:26:04 +0200

    Data-Folder;ini and reading from ini added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,19 +11,27 @@ namespace ConsoleApp1
 {
     class Program
     {   
+        static string[] months = { "Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember" };
+        static Dictionary<string, string> data = new Dictionary<string, string>();
+
         [STAThread]
         static void Main(string[] args)
         {
-            string[] months = { "Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember" };
+            if(ReadDataFromFile("./db.ini"))
+            {
+                CreateBirthDayCSV();
+                CreateBloodCSV();
+            }
+            Console.ReadKey();
+        }
+
+        private static void CreateBirthDayCSV()
+        {
             var csv = new StringBuilder();
-            csv.AppendLine("Geburtsdatum;Vorname;Nachname;Alter");
+            csv.AppendLine(data["csvHeaderBirthdate"]);
+            var queryString = data["birthdayQuery"] + DateTime.Now.Month;
 
-            Console.WriteLine("Hallo, es wird bald etwas ausgegeben!");
-            // Provider = Microsoft.Jet.OLEDB.4.0; Data Source = "C:\Users\Reg1nleifr\Desktop\Koch Ines\DBKesztele.mdb"
-            var connectionString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = \"C:\\Users\\Reg1nleifr\\Desktop\\Koch Ines\\DBKesztele.mdb\"";
-            var queryString = "select * from Debitor where Month(Birthdate) = " + DateTime.Now.Month;
-
-            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            using (OleDbConnection connection = new OleDbConnection(data["connectionString"]))
             using (OleDbCommand command = new OleDbCommand(queryString, connection))
             {
                 try
@@ -32,14 +40,13 @@ namespace ConsoleApp1
                     OleDbDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
-                        var birthdate = ((DateTime)reader["Birthdate"]);
+                        var birthdate = ((DateTime)reader[data["birthdayColName"]]);
                         var age = DateTime.Now.Year - birthdate.Year;
-                        var firstname = reader["Firstname"].ToString();
-                        var lastname = reader["Lastname"].ToString();
-                        string newLine = string.Format("{0};{1};{2};{3}", birthdate.ToShortDateString(), firstname, lastname, age);
+                        var firstname = reader[data["firstnameColName"]].ToString();
+                        var lastname = reader[data["lastnameColName"]].ToString();
+                        string newLine = string.Format("{0};{1};{2};{3}", birthdate.ToShortDateString(), lastname, firstname, age);
                         Console.WriteLine(newLine);
                         csv.AppendLine(newLine);
-
                     }
                     reader.Close();
                 }
@@ -49,46 +56,69 @@ namespace ConsoleApp1
                 }
             }
             
+            ShowCSVSaveDialog(csv, data["csvDefaultNameBirthdate"], data["csvInitialDirBirthdate"]);
+        }
+        private static void CreateBloodCSV()
+        {
+            var csv = new StringBuilder();
+            csv.AppendLine(data["csvHeaderBlood"]);
+
+            var queryString = data["bloodQuery"];
+            using (OleDbConnection connection = new OleDbConnection(data["connectionString"]))
+            using (OleDbCommand command = new OleDbCommand(queryString, connection))
+            {
+                try
+                {
+                    connection.Open();
+                    OleDbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var desc = reader[data["descriptionColName"]];
+                        var bloodTime = ((DateTime)reader[data["timeColName"]]);
+                        var firstname = reader[data["firstnameColName"]].ToString();
+                        var lastname = reader[data["lastnameColName"]].ToString();
+
+                        string newLine = string.Format("{0};{1};{2};{3}", desc, bloodTime.ToShortDateString(), lastname, firstname);
+                        Console.WriteLine(newLine);
+                        csv.AppendLine(newLine);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            ShowCSVSaveDialog(csv, data["csvDefaultNameBlood"], data["csvInitialDirBlood"]);
+        }
+
+        private static void ShowCSVSaveDialog(StringBuilder csv, string defaultName, string initialDir)
+        {
+
             using (var dialog = new SaveFileDialog())
             {
-                dialog.FileName = string.Format("Geburtstagskinder-{0}.csv", months[DateTime.Now.Month-1]);
-                dialog.InitialDirectory = "C:\\Users\\Reg1nleifr\\Desktop\\Koch Ines";
+                dialog.FileName = string.Format(defaultName, months[DateTime.Now.Month - 1]);
+                dialog.InitialDirectory = initialDir;
                 dialog.OverwritePrompt = true;
 
-                if(dialog.ShowDialog() == DialogResult.OK)
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
                     File.WriteAllText(dialog.FileName, csv.ToString());
                 }
             }
-            
-            // Aka - ur komplexer join für alle Daten nach 6 Monaten
-            queryString = @"SELECT * FROM 
-                           (RechnungArtikel ra 
-                            INNER JOIN Rechnung r ON r.ID = ra.RechnungsID)
-                            INNER JOIN Debitor d ON d.ID = int(r.DebitorID)
-                                WHERE ProductID like '00000125'
-                                AND DateDiff('m', Time, Date()) >= 6";
+        }
 
-            using (OleDbConnection connection = new OleDbConnection(connectionString))
-            using (OleDbCommand command = new OleDbCommand(queryString, connection))
+        private static bool ReadDataFromFile(string filename)
+        {
+            if(!File.Exists(filename))
             {
-                try
-                {
-                    connection.Open();
-                    OleDbDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine("Bezeichnung: {0} at {1}, by {2}", reader["Bezeichnung"].ToString(), reader["Time"], reader["Firstname"]);
-
-                    }
-                    reader.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                Console.WriteLine("Keine Datei db.ini gefunden, diese sollte im Ordner des Kasterls (Anwendung) liegen.");
+                return false;
             }
-            Console.ReadKey();
+            foreach (var row in File.ReadAllLines(filename))
+                data.Add(row.Split('=')[0], string.Join("=", row.Split('=').Skip(1).ToArray()));
+            return true;
         }
     }
 }
