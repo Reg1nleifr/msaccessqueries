147a51d60e77e9cfa6e0407479c480314b99f509
Florian Reiter <flow.reiter@gmail.com>
2017-10-24 11:08:54 +0200

    CSV raus, Apps rein

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,34 +19,42 @@ namespace ConsoleApp1
         {
             if(ReadDataFromFile("./db.ini"))
             {
-                CreateBirthDayCSV();
+                //CreateBirthDayCSV();
                 CreateBloodCSV();
             }
-            Console.ReadKey();
         }
 
         private static void CreateBirthDayCSV()
         {
             var csv = new StringBuilder();
             csv.AppendLine(data["csvHeaderBirthdate"]);
-            var queryString = data["birthdayQuery"] + DateTime.Now.Month;
+            var queryString = data["birthdayQuery"].Replace("$param$", DateTime.Now.Month.ToString());
 
-            using (OleDbConnection connection = new OleDbConnection(data["connectionString"]))
+            using (OleDbConnection connection = new OleDbConnection(data["connectionStringKesztele"]))
             using (OleDbCommand command = new OleDbCommand(queryString, connection))
             {
                 try
                 {
                     connection.Open();
+                    string newLine = "";
                     OleDbDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
                         var birthdate = ((DateTime)reader[data["birthdayColName"]]);
                         var age = DateTime.Now.Year - birthdate.Year;
-                        var firstname = reader[data["firstnameColName"]].ToString();
-                        var lastname = reader[data["lastnameColName"]].ToString();
-                        string newLine = string.Format("{0};{1};{2};{3}", birthdate.ToShortDateString(), lastname, firstname, age);
-                        Console.WriteLine(newLine);
-                        csv.AppendLine(newLine);
+                        var name = reader[data["bezeichnungColName"]].ToString().Trim();
+                        var address = reader[data["addressColName"]].ToString().Trim();
+                        var plz = reader[data["PLZColName"]].ToString().Trim();
+                        var ort = reader[data["ortColName"]].ToString().Trim();
+                        var anrede = reader[data["anredeColName"]].ToString().Trim();
+                        var old = newLine;
+
+                        newLine = string.Format("{0};{1};{2};{3};{4};{5}", birthdate.ToShortDateString(), age, name, address, plz, ort, anrede);
+                        if(old != newLine)
+                        {
+                            Console.WriteLine(newLine);
+                            csv.AppendLine(newLine);
+                        } 
                     }
                     reader.Close();
                 }
@@ -55,7 +63,6 @@ namespace ConsoleApp1
                     Console.WriteLine(ex.Message);
                 }
             }
-            
             ShowCSVSaveDialog(csv, data["csvDefaultNameBirthdate"], data["csvInitialDirBirthdate"]);
         }
         private static void CreateBloodCSV()
@@ -64,23 +71,27 @@ namespace ConsoleApp1
             csv.AppendLine(data["csvHeaderBlood"]);
 
             var queryString = data["bloodQuery"];
-            using (OleDbConnection connection = new OleDbConnection(data["connectionString"]))
+            using (OleDbConnection connection = new OleDbConnection(data["connectionStringKesztele"]))
             using (OleDbCommand command = new OleDbCommand(queryString, connection))
             {
                 try
                 {
                     connection.Open();
+                    string newLine = "";
                     OleDbDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
-                        var desc = reader[data["descriptionColName"]];
+                        var desc = reader[data["descriptionColName"]].ToString().Trim();
                         var bloodTime = ((DateTime)reader[data["timeColName"]]);
-                        var firstname = reader[data["firstnameColName"]].ToString();
-                        var lastname = reader[data["lastnameColName"]].ToString();
+                        var name = reader[data["bloodBezeichnungColName"]].ToString().Trim();
+                        var old = newLine;
 
-                        string newLine = string.Format("{0};{1};{2};{3}", desc, bloodTime.ToShortDateString(), lastname, firstname);
-                        Console.WriteLine(newLine);
-                        csv.AppendLine(newLine);
+                        newLine = string.Format("{0};{1};{2}", desc, bloodTime.ToShortDateString(), name);
+                        if (old != newLine)
+                        {
+                            Console.WriteLine(newLine);
+                            csv.AppendLine(newLine);
+                        }
                     }
                     reader.Close();
                 }
@@ -104,7 +115,7 @@ namespace ConsoleApp1
 
                 if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    File.WriteAllText(dialog.FileName, csv.ToString());
+                    File.WriteAllText(dialog.FileName, csv.ToString(), Encoding.UTF8);
                 }
             }
         }
@@ -116,8 +127,15 @@ namespace ConsoleApp1
                 Console.WriteLine("Keine Datei db.ini gefunden, diese sollte im Ordner des Kasterls (Anwendung) liegen.");
                 return false;
             }
-            foreach (var row in File.ReadAllLines(filename))
-                data.Add(row.Split('=')[0], string.Join("=", row.Split('=').Skip(1).ToArray()));
+            foreach (var row in File.ReadAllLines(filename, Encoding.UTF8))
+            {
+                string datum = string.Join("=", row.Split('=').Skip(1).ToArray());
+                if (data.ContainsKey("initdir")) //könnte man dynamisch machen :>
+                {
+                    datum = datum.Replace("$initdir$", data["initdir"]); //Initdir ersetzen!
+                }                
+                data.Add(row.Split('=')[0], datum);
+            }
             return true;
         }
     }
